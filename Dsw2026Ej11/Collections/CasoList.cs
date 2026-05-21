f5165bcf976a997f61f02525ac0c0dc146d00112
Juampykk <juanpablofrias20@gmail.com>
2026-05-21 17:33:40 -0300

    Ejercicio terminado

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,5 +1,5 @@
 ﻿namespace Dsw2026Ej11.Collections;
-
+using Dsw2026Ej11.Domain;
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
 //Incluir un método para retornar la lista
@@ -8,4 +8,19 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    public List<Alumno> ListAlumnos { get; }
+
+    public CasoList() => ListAlumnos = new List<Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        ListAlumnos.Add(alumno);
+    }
+
+    public Alumno? BuscarPorNombre(string nombre) => ListAlumnos.Find(alumno => alumno.Nombre == nombre);
+
+    public bool EliminarAlumno(Alumno alumno) => ListAlumnos.Remove(alumno);
+
+    public void EliminarAlumnoPorPosicion(int posicion) => ListAlumnos.RemoveAt(posicion);
+
 }
